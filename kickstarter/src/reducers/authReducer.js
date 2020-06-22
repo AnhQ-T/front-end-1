d1f117b6935a70fb501d7d9a15d6c8c1383f6f43
Anh Truong <aqtruong97@gmail.com>
2020-06-22 15:31:34 -0400

    Authentication Reducers initialized

---
+++ kickstarter/src/reducers/authReducer.js
@@ -0,0 +1,30 @@
+import * as authAction from '../actions/authActions';
+
+const initialState = {
+    username: '',
+    password: '',
+    error: '',
+    user_data: [],
+    isFetching: false,
+}
+
+export const LoginReducer = (state = initialState, action) => {
+    switch(action.type) {
+        case authAction.LOGIN:
+            return {
+                ...state,
+                isFetching: true,
+            }
+        case authAction.LOGIN_SUCCESS:
+            return {
+                ...state,
+                user_data: action.payload,
+                isFetching: false,
+            }
+        case authAction.REGISTER_FAILURE:
+            return {
+                ...state,
+                error: action.payload
+            }
+    }
+}
