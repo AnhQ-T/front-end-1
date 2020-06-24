3689af85bd9163b4b077689fe2dcbd32769d0dfb
Dudeicle <brian-griffiths@lambdastudents.com>
2020-06-24 18:51:13 -0400

    finished profile component

--- kickstarter-app/src/components/profile.js
+++ kickstarter-app/src/components/profile.js
@@ -9,22 +9,44 @@ const Container = styled.div`
     margin-top: 2%;
     margin-bottom: 2%;
     padding: 2%;
-    border: 1px solid gray;
-    box-shadow: -7px -5px 6px gray;
+    border: 1px solid lightgray;
+    box-shadow: -7px -5px 9px lightgray;
+`;
+const BottomContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 2%;
+`;
+const Campaigns = styled.div `
+    text-align: center;
+    margin-left: 15%;
+`;
+const StatusAndPredictions = styled.div `
+    text-align: center;
+    margin-right: 15%;
 `;
 const TopButtons = styled.div`
-    width: 15%;
-    margin-left: 85%;
+    width: 35%;
+    margin-left: 65%;
     padding: 0%;
     display: flex;
 `;
 const Button = styled.button`
     width: 50%;
-    height: 1rem;
+    height: 2rem;
+    padding: 2%;
+    text-align: center;
+    background-color: teal;
+    color: white;
+    border: none;
+    border-left: 1px solid black;
 `;
 const HOne = styled.h1`
-    font-size: 3rem;
+    font-size: 1.5rem;
     text-align: center;
+`;
+const HTwo = styled.h2`
+    font-size: 1rem;
 `;
 // const Container = styled.h1`
 //     font-size: 100px;
@@ -43,16 +65,23 @@ return (
         <div className="title">
             <HOne>Welcome</HOne>
         </div>
+    <BottomContainer className="bottomContainer">
+        <Campaigns className="campaigns">
+            <HTwo>Current Campaigns</HTwo>
+            <div>lorem</div>
+            <div>lorem</div>
+            <div>lorem</div>
+            <div>lorem</div>
+        </Campaigns>
 
-        <div className="campaigns">
-            <h2>Current Campaigns</h2>
-            <div></div>
-        </div>
-
-        <div className="statusAndPredictions">
-        
-        </div>
-
+        <StatusAndPredictions className="statusAndPredictions">
+            <HTwo>Project Status and Predictions</HTwo>
+            <div>lorem</div>
+            <div>lorem</div>
+            <div>lorem</div>
+            <div>lorem</div>
+        </StatusAndPredictions>
+    </BottomContainer>
     </Container>
 )
 }
